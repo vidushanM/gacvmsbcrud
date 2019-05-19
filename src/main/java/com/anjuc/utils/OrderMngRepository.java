b8610ca684e7514ce0df3dc485820d720c03bf8a
DasunMadushanka <43329979+DasunMadushanka@users.noreply.github.com>
2019-05-19 22:30:31 +0530

    Delete OrderMngRepository.java

--- src/main/java/com/anjuc/utils/OrderMngRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anjuc.utils.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    public Product findProductByProductCode(String ProductCode);
-    public void deleteProductById (int id);
-    public Product findProductById (int id);
-}
