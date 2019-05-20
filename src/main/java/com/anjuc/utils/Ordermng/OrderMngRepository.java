15283b8b41e226a43ac3ed55d4dc736669060b34
DasunMadushanka <43329979+DasunMadushanka@users.noreply.github.com>
2019-05-20 11:31:45 +0530

    Update OrderMngRepository.java

--- src/main/java/com/anjuc/utils/Ordermng/OrderMngRepository.java
+++ src/main/java/com/anjuc/utils/Ordermng/OrderMngRepository.java
@@ -2,7 +2,7 @@ package com.anjuc.utils.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Integer> {
+public interface OrderMngRepository extends JpaRepository<Order, Integer> {
     public Order findOrderByOrderCode(String OrderCode);
     public void deleteOrderById (int id);
     public Order findOrdersById (int id);
